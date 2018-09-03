5d87c061e78e8d08874c0ffdb9b9b247b30d5bfb
John Angelis <john_agelis@hotmail.com>
2018-09-03 15:03:50 +0300

    Changes at data file

--- data/conv.cpp
+++ data/conv.cpp
@@ -16,15 +16,28 @@ g++ conv.cpp `root-config --cflags --glibs`
 #include <ROOT/TCanvas.h>
 #include <ROOT/TCanvas.h>
 #include <ROOT/TLorentzVector.h>
+#include <ROOT/TTreeReader.h>
+#include <ROOT/TTreeReaderArray.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     TFile *inputSignal = TFile::Open("HIGGSsignal.root", "READ");
     TTree *treeS = nullptr;
     inputSignal->GetObject("tree", treeS);
-    int nEntriesS = (int)treeS->GetEntries();
+
+    int nEntries;
+    if (argc > 1)
+    {
+        nEntries = atoi(argv[1]) / 2;
+    }
+    else
+    {
+        nEntries = (int)treeS->GetEntries();
+    }
+    // int nEntriesS = (int)treeS->GetEntries();
+
     TFile *inputBkg = TFile::Open("HIGGSbackground.root", "READ");
     TTree *treeB = nullptr;
     inputBkg->GetObject("tree", treeB);
@@ -35,6 +48,26 @@ int main()
                                 "jet_1_pt", "jet_1_eta", "jet_1_phi", "jet_1_b_tag", "jet_2_pt", "jet_2_eta", "jet_2_phi", "jet_2_b_tag",
                                 "jet_3_pt", "jet_3_eta", "jet_3_phi", "jet_3_b_tag", "jet_4_pt", "jet_4_eta", "jet_4_phi", "jet_4_b_tag",
                                 "m_jj", "m_jjj", "m_lv", "m_jlv", "m_bb", "m_wbb", "m_wwbb"};
+
+    // // Set up reading from the TTree
+    // TTreeReader reader(treeS);
+    // // TTreeReaderArray<double> lep_pt(reader, "lepton_pT");
+    // vector<TTreeReaderArray<double>> readers;
+    // // double entries = reader.GetEntries(true);
+    // for (int i = 0; i < 28; i++)
+    // {
+    //     readers.emplace_back(reader, var_names[i].c_str());
+    // }
+
+    // while (reader.Next())
+    // {
+    //     reader.Next();
+    //     for (int i = 0, n = readers[0].GetSize(); i < n; ++i)
+    //     {
+    //         cout << readers[1][i] << endl;
+    //     }
+    // }
+
     for (int i = 0; i < 28; i++)
     {
         treeS->SetBranchAddress(var_names[i].c_str(), &vars[i]);
@@ -60,7 +93,7 @@ int main()
     }
     out_file << "label\n";
 
-    for (int iEntry = 0; iEntry < nEntriesS / 100; iEntry++)
+    for (int iEntry = 0; iEntry < nEntries; iEntry++)
     {
         treeS->GetEntry(iEntry);
         for (int i = 0; i < 28; i++)
@@ -70,7 +103,7 @@ int main()
         out_file << 1 << "\n";
         signalTree->Fill();
     }
-    for (int iEntry = 0; iEntry < nEntriesB / 100; iEntry++)
+    for (int iEntry = 0; iEntry < nEntries; iEntry++)
     {
         treeB->GetEntry(iEntry);
         for (int i = 0; i < 28; i++)
